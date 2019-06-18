3a17a990f2e5f5f724460db81e2ea0fb7d474197
npv34 <phanvanluan611996@gmail.com>
2019-06-18 15:09:52 +0700

    first commit

---
+++ my.js
@@ -0,0 +1,7 @@
+function clickMe() {
+    let numberOne = document.getElementById("numberOne").value;
+    let numberTwo = document.getElementById("numberTwo").value;
+    sum = parseInt(numberOne) + parseInt(numberTwo);
+    alert(sum);
+}
+
