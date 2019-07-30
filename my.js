9bd3713520af489801bab239d0ea587f9f41adc1
npv34 <phanvanluan611996@gmail.com>
2019-07-30 09:02:15 +0700

    show message with alert

--- my.js
+++ my.js
@@ -1,7 +1,3 @@
-function clickMe() {
-    let numberOne = document.getElementById("numberOne").value;
-    let numberTwo = document.getElementById("numberTwo").value;
-    sum = parseInt(numberOne) + parseInt(numberTwo);
-    alert(sum);
+function showMessage() {
+    alert("xin chao cac ban");
 }
-
